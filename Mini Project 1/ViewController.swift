8205332610e9fffbca2d5d649fccbacc7d8da66e
Sabrina Hatch <sabrinahatch222@gmail.com>
2020-07-29 16:33:44 -0400

    initial

---
+++ Mini Project 1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mini Project 1
+//
+//  Created by Sabrina Hatch on 7/23/20.
+//  Copyright © 2020 Sabrina Hatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
